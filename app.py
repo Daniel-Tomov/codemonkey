3bc2dd4df6ccd9d8eb407aedab0d77589cd3911f
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2022-09-25 10:49:03 -0400

    remove unused

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 
 from flask import Flask, jsonify, render_template, request
 import time
-from yml import items
 app = Flask(__name__)
 
 @app.route('/', methods=["POST", "GET"])
 def index():
-
-    return render_template('index.html', buttons=items)
-
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=80)
