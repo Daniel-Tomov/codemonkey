a6aa90cfc762ad1854215e06d9c08d42be011e23
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2022-12-27 05:08:04 +0000

    from replit

--- sendEmail.py
+++ sendEmail.py
@@ -4,27 +4,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-codemonkeyEmail = 'noreply@codemonkey.tk'
+codemonkeyEmail = 'daniel308712@gmail.com'
 codemonkeyPassword = os.getenv('emailPassword')
 
 to = 'daniel308712@gmail.com'
 subject = 'insert subject here'
 exampleBody = 'insert body here'
 
-#print(codemonkeyPassword)
-
 def sendEmail(userEmail, subject, body=None):
     try:
-        smtp_server = smtplib.SMTP('mail.codemonkey.tk', 587)
-        smtp_server.starttls()
-#        print("ehlo")
+        smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        smtp_server.ehlo()
         smtp_server.login(codemonkeyEmail, codemonkeyPassword)
         
         if body == None:
             message = 'Subject: {}\n\n{}'.format(subject, exampleBody)
         else:
             message = 'Subject: {}\n\n{}'.format(subject, body)
-#        print("logged in")
+
         smtp_server.sendmail(codemonkeyEmail, userEmail, message)
         
         smtp_server.close()
