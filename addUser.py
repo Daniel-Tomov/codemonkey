b83a4f7d11c53edad2fc5abb4a24ae0b7176bdd8
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2023-03-30 21:28:15 +0000

    make function to add accounts

--- addUser.py
+++ addUser.py
@@ -2,16 +2,7 @@ import courseCompletion
 import completion
 import accountManager
 
-while input("Quit? [y]/[n]") != "y":
-    username = input("Enter the username for the user: ")
-    password = input("Enter the password for the user: ")
-    email = input("Enter the email for the user: ")
-
-    for i in accountManager.accounts:
-        if i.username == username:
-            print("That username already exists!")
-            exit(0)
-
+def addAccount(username, email, password):
     account = accountManager.accountManager(username, email, password)
     completion.completion(account.uid)
     courseCompletion.courseCompletion(account.uid)
@@ -25,3 +16,16 @@ while input("Quit? [y]/[n]") != "y":
     completion.saveCompletions()
 
     print("Added the user")
+
+
+while input("Quit? [y]/[n]") != "y":
+    username = input("Enter the username for the user: ")
+    password = input("Enter the password for the user: ")
+    email = input("Enter the email for the user: ")
+
+    for i in accountManager.accounts:
+        if i.username == username:
+            print("That username already exists!")
+            exit(0)
+
+    addAccount(username, email, password)
