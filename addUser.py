b219ff1cb087fb63fc967c4df430057d26c7ecca
DANIEL TOMOV <>
2023-04-10 22:52:37 +0000

    user is not prompted on file import

--- addUser.py
+++ addUser.py
@@ -3,29 +3,27 @@ import completion
 import accountManager
 
 def addAccount(username, email, password):
-    account = accountManager.accountManager(username, email, password)
-    completion.completion(account.uid)
-    courseCompletion.courseCompletion(account.uid)
+  account = accountManager.accountManager(username, email, password)
+  completion.completion(account.uid)
+  courseCompletion.courseCompletion(account.uid)
+  # if the data in the challenges.yaml has changed, then add that information to the completion and courseCompletions for the user
+  courseCompletion.addNewCourseCompletions()
+  completion.addNewCompletions()
 
-    # if the data in the challenges.yaml has changed, then add that information to the completion and courseCompletions for the user
-    courseCompletion.addNewCourseCompletions()
-    completion.addNewCompletions()
+  accountManager.saveAccounts()
+  courseCompletion.saveCourseCompletions()
+  completion.saveCompletions()
+  print("Added the user")
 
-    accountManager.saveAccounts()
-    courseCompletion.saveCourseCompletions()
-    completion.saveCompletions()
-
-    print("Added the user")
-
-
-while input("Quit? [y]/[n]") != "y":
+if __name__ == "__main__":
+  while input("Quit? [y]/[n]") != "y":
     username = input("Enter the username for the user: ")
     password = input("Enter the password for the user: ")
     email = input("Enter the email for the user: ")
-
+   
     for i in accountManager.accounts:
-        if i.username == username:
-            print("That username already exists!")
-            exit(0)
-
+      if i.username == username:
+        print("That username already exists!")
+        exit(0)
+    
     addAccount(username, email, password)
